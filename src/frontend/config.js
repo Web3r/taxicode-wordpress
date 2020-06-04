96175d1528f65869595ca82461ab41f7c2c67dad
Alasdair Watson <alasdair@black-ink.org>
2020-06-04 14:37:55 +0100

    clean up of config

--- src/frontend/config.js
+++ src/frontend/config.js
@@ -1,9 +1,4 @@
 module.exports = {
-    TAXICODE_API_KEY: 'YEp9MPiszAlVD0oo',
-    TAXICODE_API_SECRET: 'poB7ByISTawRxHyh',
-    TAXICODE_API_KEY: 'YEp9MPiszAlVD0oo',
-    TAXICODE_API_SECRET: 'poB7ByISTawRxHyh',
     QUOTE_URL: 'https://api.taxicode.com/booking/quote/',
     PAYMENT_URL: 'https://api.taxicode.com/booking/pay/',
-    BOOKING_DETAILS_URL: 'https://api.taxicode.com/booking/details/?key=YEp9MPiszAlVD0oo&secret=poB7ByISTawRxHyh&id=',
 }
