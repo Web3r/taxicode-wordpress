cd96cee85f99312f4248e82634b49bc3769f3ab6
Alasdair Watson <alasdair@black-ink.org>
2020-06-15 15:45:02 +0100

    plugin data update

--- settings.php
+++ settings.php
@@ -1,7 +1,3 @@
-var TAXICODE_API_KEY = <?php echo 'footoballe';?>;
-var TAXICODE_API_SECRET = 'poB7ByISTawRxHyh';
-var STRIPE_API_KEY = 'YEp9MPiszAlVD0oo';
-var STRIPE_API_SECRET = 'poB7ByISTawRxHyh';
 var TAXICODE_QUOTE_URL = 'https://api.taxicode.com/booking/quote/';
 var TAXICODE_PAYMENT_URL = 'https://api.taxicode.com/booking/pay/';
 var TAXICODE_BOOKING_DETAILS_URL = 'https://api.taxicode.com/booking/details/?key=YEp9MPiszAlVD0oo&secret=poB7ByISTawRxHyh&id=';
